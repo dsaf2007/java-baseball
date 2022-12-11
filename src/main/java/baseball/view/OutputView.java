c789bda622469aaa4c6e93e6f19d7a2d44d21043
dsaf2007 <dsaf2007@naver.com>
2022-12-11 23:01:51 +0900

    formatting : 전체 코드 formatting

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,24 +1,21 @@
-package baseball;
-public class OutputView {
+package baseball.view;
 
+public class OutputView {
 
     private static String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static String INPUT_GUIDE_MESSAGE = "숫자를 입력해주세요 : ";
     private static String GAMEOVER_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
-    public static void startMessage()
-    {
+    public static void startMessage() {
         System.out.println(START_MESSAGE);
     }
 
-    public static void gameoverMessage()
-    {
+    public static void gameoverMessage() {
         System.out.println(GAMEOVER_MESSAGE);
     }
 
-    public static void inputGuideMessage()
-    {
+    public static void inputGuideMessage() {
         System.out.print(INPUT_GUIDE_MESSAGE);
     }
-    
+
 }
